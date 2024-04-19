71a56502e3d0c392ad272660d11b68fe03f6ecdf
medynski <wmedynski@gmail.com>
2024-04-19 07:28:51 +0200

    fix: adjusted overflow on mobile devices

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ const inter = Inter({ subsets: ["latin"], weight: ["400", "700"] });
 export default function Home() {
   return (
     <>
-      <div className="tb:relative tb:h-[535px] tb:w-[600px] absolute left-0 top-0 h-full w-full rounded bg-white shadow-[0px_16px_32px_0px_#1E2A3214]">
-        <div className="tb:h-32 tb:flex-row tb:justify-start flex h-[193px] flex-col items-center justify-center rounded-tl rounded-tr bg-salmon px-10">
-          <div className="tb:hidden active:bg-salmon-100 absolute right-6 top-4 cursor-pointer rounded p-2">
+      <div className="absolute left-0 top-0 h-full w-full overflow-auto rounded bg-white shadow-[0px_16px_32px_0px_#1E2A3214] tb:relative tb:h-[535px] tb:w-[600px] tb:overflow-hidden">
+        <div className="flex h-[193px] flex-col items-center justify-center rounded-tl rounded-tr bg-salmon px-10 tb:h-32 tb:flex-row tb:justify-start">
+          <div className="absolute right-6 top-4 cursor-pointer rounded p-2 active:bg-salmon-100 tb:hidden">
             <Icon name={IconName.Close} />
           </div>
-          <div className="tb:mb-0 mb-4">
+          <div className="mb-4 tb:mb-0">
             <Image
               src="/giving-block.svg"
               alt="Giving block"
@@ -23,8 +23,8 @@ export default function Home() {
               priority
             />
           </div>
-          <div className="tb:text-left pl-5 text-center">
-            <div className="tb:text-[32px] text-2xl font-semibold text-midnight-purple">
+          <div className="pl-5 text-center tb:text-left">
+            <div className="text-2xl font-semibold text-midnight-purple tb:text-[32px]">
               The giving block
             </div>
             <div
